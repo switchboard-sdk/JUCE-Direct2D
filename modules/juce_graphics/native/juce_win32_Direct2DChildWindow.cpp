5eb4e06108f68f96ae2d02ec4fba6c763aab954c
Matt Gonzalez <matt@echoaudio.com>
2023-04-10 11:15:28 -0700

    Cleaned up JUCE_DIRECT2D_PARTIAL_REPAINT

--- modules/juce_graphics/native/juce_win32_Direct2DChildWindow.cpp
+++ modules/juce_graphics/native/juce_win32_Direct2DChildWindow.cpp
@@ -120,6 +120,7 @@ namespace juce
 
                     if (SUCCEEDED(hr))
                     {
+#if JUCE_DIRECT2D_PARTIAL_REPAINT
                         if (updateRect != nullptr && allBuffersPresented)
                         {
                             RECT dirtyRectangle
@@ -140,6 +141,7 @@ namespace juce
                             hr = swapChain->Present1(presentSyncInterval, presentFlags, &presentParameters);
                         }
                         else
+#endif
                         {
                             hr = swapChain->Present(presentSyncInterval, presentFlags);
 
@@ -147,11 +149,13 @@ namespace juce
                             // Every buffer in the swap chain needs to be presented without dirty rectangles once
                             // before calling Present1 with dirty rectangles
                             // 
+#if JUCE_DIRECT2D_PARTIAL_REPAINT
                             if (!allBuffersPresented)
                             {
                                 ++numPresentedBuffers;
                                 allBuffersPresented = numPresentedBuffers >= bufferCount;
                             }
+#endif
                         }
                     }
 
@@ -209,8 +213,10 @@ namespace juce
             uint32 const swapChainFlags;
             uint32 const presentSyncInterval;
             uint32 const presentFlags;
+#if JUCE_DIRECT2D_PARTIAL_REPAINT
             bool allBuffersPresented = false;
             UINT numPresentedBuffers = 0;
+#endif
             HWND hwnd = nullptr;
             SharedResourcePointer<Direct2DFactories> factories;
             ComSmartPtr<ID2D1DeviceContext> deviceContext;
@@ -296,8 +302,10 @@ namespace juce
                                             swapChain.resetAndGetPointerAddress());
                                         if (SUCCEEDED(hr))
                                         {
+#if JUCE_DIRECT2D_PARTIAL_REPAINT
                                             allBuffersPresented = false;
                                             numPresentedBuffers = 0;
+#endif
 
                                             ComSmartPtr<ID2D1Device> direct2DDevice;
                                             hr = factories->d2dFactory->CreateDevice(dxgiDevice, direct2DDevice.resetAndGetPointerAddress());
@@ -328,8 +336,10 @@ namespace juce
                 swapChain = nullptr;
                 deviceContext = nullptr;
 
+#if JUCE_DIRECT2D_PARTIAL_REPAINT
                 allBuffersPresented = false;
                 numPresentedBuffers = 0;
+#endif
             }
 
             void createSwapChainBuffer()
