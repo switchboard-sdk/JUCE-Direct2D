9a581f49e120316c83856862e3a7e32c9634759f
Matt Gonzalez <matt@echoaudio.com>
2023-04-11 17:05:10 -0700

    Set presentSyncInterval to 0 if JUCE_WAIT_FOR_VBLANK is enabled

--- modules/juce_graphics/native/juce_win32_Direct2DChildWindow.cpp
+++ modules/juce_graphics/native/juce_win32_Direct2DChildWindow.cpp
@@ -15,7 +15,11 @@ namespace juce
                 dxgiScaling(dxgiScaling_),
                 scaleFactor(scaleFactor_),
                 swapChainFlags(0/*DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING*/),
+#if JUCE_WAIT_FOR_VBLANK
+                presentSyncInterval(0),
+#else
                 presentSyncInterval(1),
+#endif
                 presentFlags(0/*DXGI_PRESENT_ALLOW_TEARING*/)
 
 //                 swapChainFlags(tearingSupported ? DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING : 0),
